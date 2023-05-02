00a4f574ad998ebfb3f04c34320ef2c9268de099
Dave <david00100johnson@gmail.com>
2023-05-01 19:43:26 -0700

    Update: landing page, created routes, TODO : library/Sass design MVC

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import React from 'react';
+import { Switch } from 'react-router-dom';
+import { Route, Link, Routes } from 'react-router-dom';
+
+function Home() {
+  return <h1>Home</h1>;
+}
+
+function Blog() {
+  return <h1>Blog</h1>;
+}
+
+function About() {
+  return <h1>About</h1>;
+}
+
+function NotFound() {
+  return <h1>404 Not Found</h1>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/blog">Blog</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
       </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+
+      </main>
+      <footer>
+        <p>Copyright © {new Date().getFullYear()}</p>
+      </footer>
     </div>
   );
 }
