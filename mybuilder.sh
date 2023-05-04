ee40f0e18518595e7be0e189bf9249871f52f4f3
Dave <david00100johnson@gmail.com>
2023-05-03 20:19:23 -0700

    updating script and markdown

--- mybuilder.sh
+++ mybuilder.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
+# npm run deploy
+
+# npm run deploy -- -m "Deploy React app to GitHub Pages"
+
+# git add .
+
+# git commit -m 'commit with bash script'
+
+# git push
+
 npm run deploy
 
 npm run deploy -- -m "Deploy React app to GitHub Pages"
 
 git add .
 
-git commit -m 'commit with bash script'
+if [ $# -eq 0 ]
+  then
+    git commit -m 'commit with bash script'
+  else
+    git commit -m "$1"
+fi
 
 git push
+
 
