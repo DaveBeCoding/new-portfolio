d8c6913ca73028a78932a0c1dccf87e0e749d43e
Dave <david00100johnson@gmail.com>
2023-05-03 20:30:02 -0700

    cleaning up automation build script

--- mybuilder.sh
+++ mybuilder.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-# npm run deploy
-
-# npm run deploy -- -m "Deploy React app to GitHub Pages"
-
-# git add .
-
-# git commit -m 'commit with bash script'
-
-# git push
-
 npm run deploy
 
 npm run deploy -- -m "Deploy React app to GitHub Pages"
