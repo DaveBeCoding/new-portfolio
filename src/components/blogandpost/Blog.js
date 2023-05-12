ab478f0c3ad476d5291bb3ce682fee97f4172541
Dave <david00100johnson@gmail.com>
2023-05-12 01:50:35 -0700

    Updating blog sample entry

--- src/components/blogandpost/Blog.js
+++ src/components/blogandpost/Blog.js
@@ -35,26 +35,9 @@ const post = [
   {
     title: 'Hybrid Encryption',
     author: 'Dave',
-    content: 'This is the content of the first post,This is the content of the first post' 
-    +'This is the content of the first post.This is the content of the first post. '
-    +'This is the content of the first post.This is the content of the first post. '
-    +'This is the content of the first post.This is the content of the first post. '
-    +'This is the content of the first post.This is the content of the first post. '
-    +'This is the content of the first post.This is the content of the first post. '
-    +'This is the content of the first post.This is the content of the first post. '
-    +'This is the content of the first post.This is the content of the first post. '
-    +'This is the content of the first post.This is the content of the first post. '
-    +'This is the content of the first post.This is the content of the first post. '
-    +'This is the content of the first post.This is the content of the first post. '
-    +'This is the content of the first post.This is the content of the first post. '
-    +'This is the content of the first post.This is the content of the first post. '
-    +'This is the content of the first post.This is the content of the first post. '
-    +'This is the content of the first post.This is the content of the first post. '
-    +'This is the content of the first post.This is the content of the first post. '
-    +'This is the content of the first post.This is the content of the first post. '
-    +'This is the content of the first post.This is the content of the first post. '
-    +'This is the content of the first post.This is the content of the first post. '
-    +'This is the content of the first post.This is the content of the first post. ',
+    content: 'A 256-bit size is represented as 2^256.This number represents the total number of possible keys that could be used in AES-256 encryption. This is a very large number, specifically:'
+    +'115,792,089,237,316,195,423,570,985,008,687,907,853,269,984,665,640,564,039,457,584,007,913,129,639,936'
+    +'The large number of possible keys makes AES-256 very secure against brute-force attacks, as the time required to test all possible keys is beyond current computational capabilities.',
     image: image2,
     video: video2,
   },
--- src/components/mainpages/About.js
+++ src/components/mainpages/About.js
@@ -55,7 +55,22 @@ function About() {
         <motion.h1 variants={mVariants} style={{ display: "inline-block", marginRight: "10px" }}>u</motion.h1>
         <motion.h1 variants={eVariants} style={{ display: "inline-block", marginRight: "10px" }}>t</motion.h1>
         <Resume/>
-        <p>Epsom is a market town in Surrey, England, known for its famous Epsom Downs Racecourse and the annual Epsom Derby. The town also has a rich history, with evidence of human settlement dating back to the Roman times. Today, Epsom is a thriving town with a bustling high street, beautiful parks and green spaces, and excellent transport links to London and other parts of the country.</p>
+        <p>Epsom is a market town in Surrey, England, known for its famous Epsom Downs Racecourse and the annual Epsom Derby. 
+          The town also has a rich history, with evidence of human settlement dating back to the Roman times. Today, Epsom is a 
+          thriving town with a bustling high street, beautiful parks and green spaces, and excellent transport links to London and 
+          other parts of the country.Epsom is a market town in Surrey, England, known for its famous Epsom Downs Racecourse and the 
+          annual Epsom Derby. The town also has a rich history, with evidence of human settlement dating back to the Roman times. Today, 
+          Epsom is a thriving town with a bustling high street, beautiful parks and green spaces, and excellent transport links to London 
+          and other parts of the country.Epsom is a market town in Surrey, England, known for its famous Epsom Downs Racecourse and the annual 
+          Epsom Derby. The town also has a rich history, with evidence of human settlement dating back to the Roman times. Today, Epsom is a thriving 
+          town with a bustling high street, beautiful parks and green spaces, and excellent transport links to London and other parts of the country.
+          Epsom is a market town in Surrey, England, known for its famous Epsom Downs Racecourse and the annual Epsom Derby. The town also has a rich history, 
+          with evidence of human settlement dating back to the Roman times. Today, Epsom is a thriving town with a bustling high street, beautiful parks and green 
+          spaces, and excellent transport links to London and other parts of the country.Epsom is a market town in Surrey, England, known for its famous Epsom Downs 
+          Racecourse and the annual Epsom Derby. The town also has a rich history, with evidence of human settlement dating back to the Roman times. Today, Epsom is a thriving 
+          town with a bustling high street, beautiful parks and green spaces, and excellent transport links to London and other parts of the country.Epsom is a market town in Surrey, 
+          England, known for its famous Epsom Downs Racecourse and the annual Epsom Derby. The town also has a rich history, with evidence of human settlement dating back to the Roman times. 
+          Today, Epsom is a thriving town with a bustling high street, beautiful parks and green spaces, and excellent transport links to London and other parts of the country.</p>
         <div className="empty-space-About"></div>
         
       </motion.div>
